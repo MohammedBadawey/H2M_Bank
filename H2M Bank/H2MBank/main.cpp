0af81ba56a4d7014641856935af9b7c3d3f6043a
MohammedBadawey <MohammedMBadawey11@gmail.com>
2024-08-13 20:02:43 +0300

    V0.0

---
+++ H2M Bank/H2MBank/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <exception>
+
+
+using namespace std;
+
+
+
+
+
+
+
+
+int main()
+{
+    cout << "Hello Ahmed!" << endl;
+    return 0;
+}
