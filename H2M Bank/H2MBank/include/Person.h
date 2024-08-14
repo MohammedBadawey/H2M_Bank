32b38427e4a506ea2263b696f105c4ac3473085b
MohammedBadawey <MohammedMBadawey11@gmail.com>
2024-08-14 14:58:52 +0300

    V0.001

--- H2M Bank/H2MBank/include/Person.h
+++ H2M Bank/H2MBank/include/Person.h
@@ -9,16 +9,57 @@ using namespace std;
 class Person{
 protected:
 	// att
-
+    string name;
+    int id;
+    string password;
 public:
 	// cons
-
+    Person(){
+    int id =0;}
+    Person (string name , int id, string password)
+    this-> name = name;
+    this->id =id;
+    this->password=password;
 	//setter
-
+    void steName(string name)
+    {
+        this->name=name;
+    }
+    void SetId(int id)
+    {
+        this->id=id;
+    }
+    void setPassword(string password)
+    {
+        this->password=password;
+    }
+    void setPersonData(string name,int id,string password)
+    {
+        this->name=name;
+        this->id=id;
+        this->password=password;
+    }
 	//getter
+    string getName()
+	{
+	    return name;
+	}
+	int getId()
+	{
+	    return id;
+	}
+	string getPassword()
+	{
+	    return password;
+	}
 
 	//method
-
+    void Printinfo()
+    {
+        cout << "Name -> " << name << endl;
+        cout << "Id -> " << id << endl;
+        cout << "Password" << "******" << endl;
+    }
 };
 
 #endif // PERSON_H
