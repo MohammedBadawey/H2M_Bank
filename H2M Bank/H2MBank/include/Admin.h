cdfb27480a2f8aa792a85470ee272dc8ac65979e
MohammedBadawey <MohammedMBadawey11@gmail.com>
2024-08-17 00:19:48 +0300

    V1.2

--- H2M Bank/H2MBank/include/Admin.h
+++ H2M Bank/H2MBank/include/Admin.h
@@ -11,33 +11,15 @@ using namespace std;
 
 class Admin : public Employee
 {
-private:
-    static Admin* instance;
+public:
     Admin(string name, int id, string password, double salary)
         : Employee(name, id, password, salary)
     {}
-
-public:
-    static Admin* getInstance(string name, int id, string password, double salary)
-    {
-        if (instance == nullptr)
-        {
-            instance = new Admin(name, id, password, salary);
-        }
-        return instance;
-    }
 
     void Display()
     {
         Employee::Display();
     }
-
-    ~Admin()
-    {
-        delete instance;
-        instance = nullptr;
-    }
 };
-
 
 #endif // ADMIN_H
